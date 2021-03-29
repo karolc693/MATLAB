23596a417a17a7f09ef6299b61a3b9852c67f25f
karolc693 <81568260+karolc693@users.noreply.github.com>
2021-03-29 15:04:55 +0200

    Add files via upload

--- PRZYKLAD129032021.m
+++ PRZYKLAD129032021.m
@@ -1,7 +1,9 @@
 N=6;
-D0=4*eye(N);
-D1=diag(-2*ones(N-1,1), 1);
-D2=diag(2*ones(N-1,1), -1);
-D3= diag(ones(N-2,1), 2);
-D4= diag(ones(N-2,1),-2);
-A=D0+D1+D2+D3+D4 
+D0=0*eye(N);
+D1=diag(1*ones(N-1,1), 1);
+D2=diag(-1*ones(N-1,1), -1);
+A=D0+D1+D2
+A(1,1)=[2]
+A(1,2)=[-2]
+A(N,N-1)=[-2]
+A(N,N)=[2]
