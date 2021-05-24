c21ed0fadcbd1a2f90ef3b679a57f8c00aa2c4f8
karolc693 <81568260+karolc693@users.noreply.github.com>
2021-05-24 21:42:33 +0200

    Add files via upload

---
+++ laborki2405212.m
@@ -0,0 +1,21 @@
+%filename is where I am getting the sound wav file from on my cpu, 
+[y,Fs] = audioread('miauczenies.wav');
+whos y
+sound(y,Fs);
+
+
+figure(1);
+clf;
+subplot(2,1,1); plot(y);
+Ts = 1/50;
+t = 0:Ts:10-Ts;   
+Y = fft(y);   
+fs = 1/Ts;
+f = (0:length(Y)-1)*fs/length(Y);
+
+Y=fftshift(Y)
+subplot(2,1,2); plot(f,abs(Y))
+xlabel('Frequency (Hz)')
+ylabel('Magnitude')
+title('Magnitude')
+
